d639342efcdb231b28d633152723592dd2f79fe0
Kalon Hinds <kh.cubed@gmail.com>
2012-02-22 16:58:04 -0500

    makin' this sorta work

--- detest.py
+++ detest.py
@@ -5,29 +5,29 @@ novel = VisualNovel("theboywholived.db")
 currentScene = novel.get_scene(novel.current_scene)
 
 while(currentScene is not None):
-	currentDialogue = Dialogue(currentScene.initial_dialogue)
+	currentDialogue = currentScene.initial_dialogue
 	print currentScene.setup_text + "\n"
 	while(currentDialogue is not None):
-		print "%s: %s" % (Actor(currentDialogue.actor).name, currentDialogue.text)
+		print "%s: %s" % (currentDialogue.actor.name, currentDialogue.text)
 		option = "0"
 		
 		if (len(currentDialogue.options) > 1):
 			for i in len(currentDialogue.options):
-				print "%s: %s" % (i, Option(currentDialogue.options[i]).display_text)
+				print "%s: %s" % (i, currentDialogue.options[i].display_text)
 			option = raw_input()
 		else:
-			print Option(currentDialogue.options[0]).display_text
+			print currentDialogue.options[0].display_text
 			raw_input()
 
 		choiceInd = int(option)
-		userOption = Option(currentDialogue.options[choiceInd])
+		userOption = currentDialogue.options[choiceInd]
 		print userOption.text
 		if(userOption.outcome_type == Outcome.DIALOGUE):
-			currentDialogue = novel.getDialogue(userOption.outcome_ref)
+			currentDialogue = novel.get_dialogue(userOption.outcome_ref)
 		elif (userOption.outcome_type == Outcome.SCENE):
 			currentDialogue = None
-			currentScene = novel.getScene(userOption.outcome_ref)
+			currentScene = novel.get_scene(userOption.outcome_ref)
 		elif (userOption.outcome_type == Outcome.GAMEEND):
 			currentDialogue = None
 			currentScene = None
-	print "The End"
+print "The End"
